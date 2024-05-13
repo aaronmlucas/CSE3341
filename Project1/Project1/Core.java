756fb2cde61750fbc7b9898942a035cf33477eeb
Aaron Lucas <aaron-lucas@insight.rr.com>
2024-05-13 11:56:40 -0400

    Project 1 Initialization
    Contains the template and test files provided for Project 1.

---
+++ Project1/Project1/Core.java
@@ -0,0 +1,43 @@
+// This enum serves as our tokens
+enum Core {
+    // Keywords
+    AND,
+    BEGIN,
+    DO,
+    END,
+    ELSE,
+    IS,
+    IF,
+    IN,
+    INTEGER,
+    NEW,
+    NOT,
+    OR,
+    OUT,
+    OBJECT,
+    PROCEDURE,
+    RETURN,
+    THEN,
+    WHILE,
+    // Symbols
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    ASSIGN,
+    EQUAL,
+    LESS,
+    COLON,
+    SEMICOLON,
+    PERIOD,
+    COMMA,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    // Others
+    CONST,
+    ID,
+    EOS,
+    ERROR
+}
